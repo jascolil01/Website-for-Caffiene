de92a58e93c65667570d412771eebdf6702b1746
jascolil01 <jayasco246@gmail.com>
2023-02-09 16:45:20 -0500

    made changes to server.js

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const routes = require('./routes');
 const db = require('./db');
 const logger = require('morgan');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 
+const db = require("./db")
+
+const { DrinkType } = require('./models/drinkType')
+
 const app = express();
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 
 app.use('/api', routes);
