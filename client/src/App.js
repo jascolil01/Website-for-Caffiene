8b84e8862024cf31ee3abd0676b7725e6fcb0880
jascolil01 <jayasco246@gmail.com>
2023-02-09 16:31:34 -0500

    implements lesson react form into code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
-import testForm from '../components/testForm'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import Form from './components/Form'
 
-function App() {
+const App = () => {
+  const [issues, setIssues] = useState([])
+
+  const getIssues = async () => {
+    try {
+      let res = await axios.get('http://localhost:3001/issues')
+      setIssues(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getIssues()
+  }, [])
+
   return (
-    <Form getIssues={getIssues} />
-  );
+    <div>
+      <Form getIssues={getIssues} />
+      <h1>Issues:</h1>
+      {issues.map((issue) => (
+        <div key={issue._id}>
+          <h3>Type: {issue.issueType}</h3>
+          <p>Subject: {issue.subject}</p>
+          <p>Message: {issue.message}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default App;
+export default App
