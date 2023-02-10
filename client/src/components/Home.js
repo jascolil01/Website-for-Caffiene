a06a36c2567e8dab86229e2af442845e0636bbab
jascolil01 <jayasco246@gmail.com>
2023-02-10 09:46:41 -0500

    added text to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default function Home() {
+export default function () {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to {"insert name later"}</h1>
+      <h2>On this website you will be able to look up you favorite kinds of caffienated drinks!</h2>
+      <h3>Can't find a drink you like? simple fill out this form and add it to our list!</h3>
+      <h4>Like a drink you see? Leave a comment on it and let others know!</h4>
+    </div>
   )
 }
