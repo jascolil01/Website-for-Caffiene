8f9587b3851d21df3f217b0757bad9873a95ee4f
jascolil01 <jayasco246@gmail.com>
2023-02-10 09:32:41 -0500

    tested export functions for components

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import About from './components/About'
+import CaffieneForm from './components/CaffieneForm'
+import CaffieneType from './components/CaffieneType'
+import DrinkForm from './components/DrinkForm'
+import Drinks from './components/Drinks'
+import NavBar from './components/NavBar'
+
+export default function App() {
+  return (
+  <div>
+    <About />
+    <CaffieneForm />
+    <CaffieneType />
+    <DrinkForm />
+    <Drinks />
+    <NavBar />
+  </div>
+  
+
+  )
+}
