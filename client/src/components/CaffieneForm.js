6616695c573678a1f85d42210be44c19044caf31
jascolil01 <jayasco246@gmail.com>
2023-02-10 09:26:34 -0500

    added basic boilerplate to new components

---
+++ client/src/components/CaffieneForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CaffieneForm() {
+  return (
+    <div>CaffieneForm</div>
+  )
+}
---
+++ client/src/components/CaffieneType.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CaffieneType() {
+  return (
+    <div>CaffieneType</div>
+  )
+}
