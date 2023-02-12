2f781c1f660bc74feb55b9fa1de4a0c135e5a061
jascolil01 <jayasco246@gmail.com>
2023-02-11 20:47:05 -0500

    reworked addDrinks.js

--- client/src/components/AddDrink.js
+++ client/src/components/AddDrink.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function AddDrink() {
   return (
-    <div>AddDrink</div>
+    <form>
+      <label htmlFor='typeOfCaffiene'>Name of the Drink</label>
+      <select id ="typeOfCaffiene">
+        <option value="Coffee">Coffee</option>
+        <option value="Tea">Tea</option>
+        <option value="Energy Drink">Energy Drink</option>
+        <option value="Espresso">Espresso</option>
+        <option value="Soda">Soda</option>
+      </select>
+      <label htmlFor='name'>Name of the Drink</label>
+      <input type="text" id="name"/>
+      <label htmlFor='description'>Tell us about the drink</label>
+      <input type="text" id="description"/>
+      <textarea id="description" cols="30" rows="10"></textarea>
+      <button type='submit'>Brew it up!</button>
+    </form>
   )
 }
